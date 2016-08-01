6db48809da694411238f6c220ad0339ec661a859
wuwentao <wwu@genowise.com>
2016-08-01 11:20:59 +0800

    - Add more tests

---
+++ test/TestClass2/Sub/Test3.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuwentao
+ * Date: 2016/8/1
+ * Time: 11:17
+ */
+
+namespace Test\TestClass2\Sub;
+
+
+class Test3
+{
+
+    public function getName()
+    {
+        return 'test3';
+    }
+}
--- test/TestLoader.php
+++ test/TestLoader.php
@@ -15,6 +15,7 @@ class TestLoader extends PHPUnit_Framework_TestCase
         require_once '../src/Loader.php';
         $register_path =[
             ['TestClass', 'TestClass', true],
+            ['Test\\TestClass2', 'TestClass2', true],
         ];
         $loader = new \Wwtg99\ClassLoader\Loader(__DIR__, $register_path);
         $loader->autoload();
@@ -22,5 +23,7 @@ class TestLoader extends PHPUnit_Framework_TestCase
         $this->assertEquals('test1', $test1->getName());
         $test2 = new \TestClass\Sub\Test2();
         $this->assertEquals('test2', $test2->getName());
+        $test3 = new \Test\TestClass2\Sub\Test3();
+        $this->assertEquals('test3', $test3->getName());
     }
 }
